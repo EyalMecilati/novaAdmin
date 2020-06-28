01c9cb6acc0c94d19a5df40f3d7e7c6237b084e5
eyal <eyal.m2552@gmail.com>
2020-06-28 19:05:37 +0300

    add async handller

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Nova\Stats;
+// use App\Nova\Stats;
 use GuzzleHttp\Client;
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Http;
+// use Illuminate\Support\Facades\Http;
+use Psr\Http\Message\ResponseInterface;
+use GuzzleHttp\Exception\RequestException;
 
 use function GuzzleHttp\json_decode;
 
@@ -15,12 +17,14 @@ class StatsController extends Controller
 
     public function getStatsData()
     {
-        $client = new \GuzzleHttp\Client();
-        $response = $client->request('GET', 'http://api.rtbravo.com/api/statsv2?token=4eyrn7z923f75admnazufvucvskm3vpyqn4422zc&from=2020-04-21&to=2020-04-25&groupby=day:paid:sid&format=json');
-        $data = json_decode($response->getBody()->getContents(),true);       
-        $time_now = date("Y-m-d");
         DB::table('stats')->delete();
-        foreach($data as $item)
+        $client = new Client();
+        $promise = $client->requestAsync('GET','http://api.rtbravo.com/api/statsv2?token=4eyrn7z923f75admnazufvucvskm3vpyqn4422zc&from=2020-04-21&to=2020-04-25&groupby=day:paid:sid&format=json');
+        $promise->then(
+            function (ResponseInterface $res) {
+                $data = json_decode($res->getBody()->getContents(),true);       
+                $time_now = date("Y-m-d");
+                foreach($data as $item)
             {
                 DB::table('stats')->insert([
                                             'day'      => $item["day"],
@@ -34,7 +38,14 @@ class StatsController extends Controller
                                             'updated_at' => date($time_now),
                                         ]);
             }
+            return view('nova');
+            },
+            function (RequestException $e) {
+               
+            }
+        )->wait();
         
+        return view('welcome');
     }
 
 }
