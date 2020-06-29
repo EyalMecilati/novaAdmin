a6d60b495569eac01d2da7b2e3ebefcbe3580382
eyal <eyal.m2552@gmail.com>
2020-06-29 17:50:46 +0300

    update every minute1

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,45 +30,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
         $schedule->command('command:callapi')->everyMinute();
-        // $schedule->call(function () {
-          
-        //     DB::disableQueryLog();
-        //     DB::table('stats')->delete();
-        //     $client = new Client();
-        //     $promise = $client->requestAsync('GET','http://api.rtbravo.com/api/statsv2?token=4eyrn7z923f75admnazufvucvskm3vpyqn4422zc&from=2020-04-21&to=2020-04-25&groupby=day:paid:sid&format=json');
-        //     $promise->then(
-        //         function (ResponseInterface $res) {
-        //             $data = json_decode($res->getBody()->getContents(),true);       
-        //             $time_now = date("Y-m-d");
-    
-        //         foreach (array_chunk($data, 1000) as $responseChunk)
-        //         {
-        //             $insertableArray = [];
-        //             foreach($responseChunk as $value) {
-        //                 $insertableArray[] = [
-        //                     'day'      => $value["day"],
-        //                     'paid'     => $value["paid"],
-        //                     'sid'      => $value["sid"],
-        //                     'requests' => $value["requests"],
-        //                     'bids'     => $value["bids"],
-        //                     'wins'     => $value["wins"],
-        //                     'cost'     => $value["cost"],
-        //                     'created_at' => date($time_now),
-        //                     'updated_at' => date($time_now),
-        //                 ];
-        //             }
-        //             DB::table('stats')->insert($insertableArray);
-        //         }
-        //         return view('welcome');
-        //         },
-        //         function (RequestException $e) {
-        //             return view('welcome');
-        //         }
-        //     )->wait();
-
-        // })->daily();
     }
 
     /**
--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -12,38 +12,38 @@ class StatsController extends Controller
 
     public function getStatsData()
     {
-        // DB::table('stats')->delete();
-        // $client = new Client();
-        // $promise = $client->requestAsync('GET','http://api.rtbravo.com/api/statsv2?token=4eyrn7z923f75admnazufvucvskm3vpyqn4422zc&from=2020-04-21&to=2020-04-25&groupby=day:paid:sid&format=json');
-        // $promise->then(
-        //     function (ResponseInterface $res) {
-        //         $data = json_decode($res->getBody()->getContents(),true);       
-        //         $time_now = date("Y-m-d");
+        DB::table('stats')->delete();
+        $client = new Client();
+        $promise = $client->requestAsync('GET','http://api.rtbravo.com/api/statsv2?token=4eyrn7z923f75admnazufvucvskm3vpyqn4422zc&from=2020-04-21&to=2020-04-25&groupby=day:paid:sid&format=json');
+        $promise->then(
+            function (ResponseInterface $res) {
+                $data = json_decode($res->getBody()->getContents(),true);       
+                $time_now = date("Y-m-d");
 
-        //     foreach (array_chunk($data, 1000) as $responseChunk)
-        //     {
-        //         $insertableArray = [];
-        //         foreach($responseChunk as $value) {
-        //             $insertableArray[] = [
-        //                 'day'      => $value["day"],
-        //                 'paid'     => $value["paid"],
-        //                 'sid'      => $value["sid"],
-        //                 'requests' => $value["requests"],
-        //                 'bids'     => $value["bids"],
-        //                 'wins'     => $value["wins"],
-        //                 'cost'     => $value["cost"],
-        //                 'created_at' => date($time_now),
-        //                 'updated_at' => date($time_now),
-        //             ];
-        //         }
-        //         DB::table('stats')->insert($insertableArray);
-        //     }
-        //     return view('welcome');
-        //     },
-        //     function (RequestException $e) {
-        //         return view('welcome');
-        //     }
-        // )->wait();
+            foreach (array_chunk($data, 1000) as $responseChunk)
+            {
+                $insertableArray = [];
+                foreach($responseChunk as $value) {
+                    $insertableArray[] = [
+                        'day'      => $value["day"],
+                        'paid'     => $value["paid"],
+                        'sid'      => $value["sid"],
+                        'requests' => $value["requests"],
+                        'bids'     => $value["bids"],
+                        'wins'     => $value["wins"],
+                        'cost'     => $value["cost"],
+                        'created_at' => date($time_now),
+                        'updated_at' => date($time_now),
+                    ];
+                }
+                DB::table('stats')->insert($insertableArray);
+            }
+            return view('welcome');
+            },
+            function (RequestException $e) {
+                return view('welcome');
+            }
+        )->wait();
         
         return view('welcome');
     }
