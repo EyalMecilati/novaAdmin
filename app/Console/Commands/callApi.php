ee1c9cfa379f7a900b4dc1363d0f21556eb1c40a
eyal <eyal.m2552@gmail.com>
2020-06-29 17:45:37 +0300

    update every minute

---
+++ app/Console/Commands/callApi.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Console\Commands;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Psr\Http\Message\ResponseInterface;
+
+class callApi extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:callapi';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        DB::table('stats')->delete();
+        $client = new Client();
+        $promise = $client->requestAsync('GET','http://api.rtbravo.com/api/statsv2?token=4eyrn7z923f75admnazufvucvskm3vpyqn4422zc&from=2020-04-21&to=2020-04-25&groupby=day:paid:sid&format=json');
+        $promise->then(
+            function (ResponseInterface $res) {
+                $data = json_decode($res->getBody()->getContents(),true);       
+                $time_now = date("Y-m-d");
+
+            foreach (array_chunk($data, 1000) as $responseChunk)
+            {
+                $insertableArray = [];
+                foreach($responseChunk as $value) {
+                    $insertableArray[] = [
+                        'day'      => $value["day"],
+                        'paid'     => $value["paid"],
+                        'sid'      => $value["sid"],
+                        'requests' => $value["requests"],
+                        'bids'     => $value["bids"],
+                        'wins'     => $value["wins"],
+                        'cost'     => $value["cost"],
+                        'created_at' => date($time_now),
+                        'updated_at' => date($time_now),
+                    ];
+                }
+                DB::table('stats')->insert($insertableArray);
+            }
+            return view('welcome');
+            },
+            function (RequestException $e) {
+                return view('welcome');
+            }
+        )->wait();
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,12 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\DB;
+use Psr\Http\Message\ResponseInterface;
+use GuzzleHttp\Exception\RequestException;
+
+use function GuzzleHttp\json_decode;
 
 class Kernel extends ConsoleKernel
 {
@@ -13,7 +19,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        \App\Console\Commands\callApi::class
     ];
 
     /**
@@ -25,6 +31,44 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('command:callapi')->everyMinute();
+        // $schedule->call(function () {
+          
+        //     DB::disableQueryLog();
+        //     DB::table('stats')->delete();
+        //     $client = new Client();
+        //     $promise = $client->requestAsync('GET','http://api.rtbravo.com/api/statsv2?token=4eyrn7z923f75admnazufvucvskm3vpyqn4422zc&from=2020-04-21&to=2020-04-25&groupby=day:paid:sid&format=json');
+        //     $promise->then(
+        //         function (ResponseInterface $res) {
+        //             $data = json_decode($res->getBody()->getContents(),true);       
+        //             $time_now = date("Y-m-d");
+    
+        //         foreach (array_chunk($data, 1000) as $responseChunk)
+        //         {
+        //             $insertableArray = [];
+        //             foreach($responseChunk as $value) {
+        //                 $insertableArray[] = [
+        //                     'day'      => $value["day"],
+        //                     'paid'     => $value["paid"],
+        //                     'sid'      => $value["sid"],
+        //                     'requests' => $value["requests"],
+        //                     'bids'     => $value["bids"],
+        //                     'wins'     => $value["wins"],
+        //                     'cost'     => $value["cost"],
+        //                     'created_at' => date($time_now),
+        //                     'updated_at' => date($time_now),
+        //                 ];
+        //             }
+        //             DB::table('stats')->insert($insertableArray);
+        //         }
+        //         return view('welcome');
+        //         },
+        //         function (RequestException $e) {
+        //             return view('welcome');
+        //         }
+        //     )->wait();
+
+        // })->daily();
     }
 
     /**
